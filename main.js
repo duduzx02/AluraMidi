34ac7f427d41daa79384423fd3df73edde4f7f6b
duduzx02 <duduzx02@gmail.com>
2022-12-16 04:15:51 -0300

    Adicionando som aos botões

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
 function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-document.querySelector('.tecla_pom');
+function tocaSomClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+function tocaSomTim() {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+function tocaSomPuff() {
+    document.querySelector('#som_tecla_puff').play();
+}
+
+function tocaSomSplash() {
+    document.querySelector('#som_tecla_splash').play();
+}
+
+function tocaSomToim() {
+    document.querySelector('#som_tecla_toim').play();
+}
+
+function tocaSomPsh() {
+    document.querySelector('#som_tecla_psh').play();
+}
+
+function tocaSomTic() {
+    document.querySelector('#som_tecla_tic').play();
+}
+
+function tocaSomTom() {
+    document.querySelector('#som_tecla_tom').play();
+}
+
+
