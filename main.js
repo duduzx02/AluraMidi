4bdc8c447aebc7b6d90153eff26849552e0f45df
duduzx02 <duduzx02@gmail.com>
2022-12-17 11:14:45 -0300

    pecorrendo uma lista

--- main.js
+++ main.js
@@ -4,5 +4,13 @@ function tocaSomPom() {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom
+let contador = 0
+
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSomPom
+
+    contador = contador + 1
+
+    console.log(contador)
+}
 
