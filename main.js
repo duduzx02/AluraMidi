b49427af5265132ae288c59d37d4cb9233004085
duduzx02 <duduzx02@gmail.com>
2022-12-17 10:52:37 -0300

    Adicionando os elementos de class tecla a uma lista no javaScript

--- main.js
+++ main.js
@@ -1,39 +1,8 @@
 function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play();
-}
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-function tocaSomTim() {
-    document.querySelector('#som_tecla_tim').play();
-}
-
-function tocaSomPuff() {
-    document.querySelector('#som_tecla_puff').play();
-}
-
-function tocaSomSplash() {
-    document.querySelector('#som_tecla_splash').play();
-}
-
-function tocaSomToim() {
-    document.querySelector('#som_tecla_toim').play();
-}
-
-function tocaSomPsh() {
-    document.querySelector('#som_tecla_psh').play();
-}
-
-function tocaSomTic() {
-    document.querySelector('#som_tecla_tic').play();
-}
-
-function tocaSomTom() {
-    document.querySelector('#som_tecla_tom').play();
-}
-
+listaDeTeclas[0].onclick = tocaSomPom
 
