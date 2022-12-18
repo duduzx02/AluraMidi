c239d33afd14d6e77667d8c117bc44341646a596
duduzx02 <duduzx02@gmail.com>
2022-12-17 22:30:11 -0300

    Adicionando uma nova classe a um elemento com JavaScript

--- main.js
+++ main.js
@@ -13,5 +13,8 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
   tecla.onclick = function () {
     tocaSom(idAudio);
   };
-  
+
+  tecla.onkeydown = function () {
+    tecla.classList.add("ativa");
+  };
 }
