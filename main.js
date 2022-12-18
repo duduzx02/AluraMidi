42187297eee17b1ab30b9debc23878c61fc0750b
duduzx02 <duduzx02@gmail.com>
2022-12-17 22:13:00 -0300

    Repetição otimizada com For

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ function tocaSom(idElementoSom) {
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
   const tecla = listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
 
@@ -15,6 +13,5 @@ while (contador < listaDeTeclas.length) {
   tecla.onclick = function () {
     tocaSom(idAudio);
   };
-
-  contador = contador + 1;
+  
 }
