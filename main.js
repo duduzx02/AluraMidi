64922739fb06406f8d555aa74162ffd33ce65ab0
duduzx02 <duduzx02@gmail.com>
2022-12-17 21:37:50 -0300

    function anônimas

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ const listaDeTeclas = document.querySelectorAll(".tecla");
 let contador = 0;
 
 while (contador < listaDeTeclas.length) {
-  listaDeTeclas[contador].onclick = tocaSom;
+  listaDeTeclas[contador].onclick = function(){
+    tocaSom('#som_tecla_pom')
+  };
 
   contador = contador + 1;
 
